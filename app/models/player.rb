af101d9ec40c9a853ee3410966a90a39ddcc545a
RVirk04 <randeepvirk29@gmail.com>
2022-03-01 12:24:15 -0600

    Added the validations along with the associations to the generated models.

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,7 @@
 class Player < ApplicationRecord
   belongs_to :country
+
+  validates :name, :position, :overall, presence: true
+  validates :name, uniqueness: true
+  validates :overall, numericality: {only_integer: true}
 end
