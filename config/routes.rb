5696fb96712bc8f5ac632983f4d7849e63384517
RVirk04 <randeepvirk29@gmail.com>
2022-03-01 13:28:21 -0600

    Modified the routes.rb file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  # get 'countries/index'
+  # get 'countries/show'
+  # get 'players/index'
+  # get 'players/show'
+  resources :countries, only: [:index, :show]
+  resources :players, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
 end
