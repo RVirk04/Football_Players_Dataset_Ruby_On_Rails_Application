fd90360a39d39a6942e1802e8fb0e6b7781c375e
RVirk04 <randeepvirk29@gmail.com>
2022-03-03 15:23:45 -0600

    Added the search functionality to the countries index.

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,8 +1,19 @@
 class CountriesController < ApplicationController
   def index
+    # @q = Country.ransack(params[:q])
     @countries = Country.all.paginate(page: params[:page], per_page: 10)
   end
 
   def show
   end
+
+  def search
+    @query = params[:query]
+    @countries = Country.paginate(page: params[:page], per_page: 10).where("countries.name LIKE ?",["%#{@query}%"])
+    render "index"
+    if @query.empty?
+      flash[:notice] = "No results found for #{params[:query]}"
+    end
+  end
+
 end
